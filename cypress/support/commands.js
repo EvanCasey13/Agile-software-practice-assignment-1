f67fd19494879a2553c43e97ec54f6324766863d
EvanCasey13 <evancasey2003@gmail.com>
2022-11-14 13:37:02 +0000

    Updated review test to use custom commands.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,42 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('checkRedHeartExists', (index) => {
+    cy.get(".MuiCardHeader-root").eq(index).find("svg").should("not.exist");
+    cy.get("button[aria-label='add to favorites']").eq(index).click();
+    cy.get(".MuiCardHeader-root").eq(index).find("svg");
+  });
+
+  Cypress.Commands.add('addToFavourites', (index) => {
+      cy.get("button[aria-label='add to favorites']").eq(index).click();
+      cy.get("button[aria-label='add to favorites']").eq(index).click();
+  });
+
+  Cypress.Commands.add('clickButton', (label) => {
+    cy.get("button").contains(label).click();
+});
+
+Cypress.Commands.add('checkIfMoviesAreListed', (index, title) => {
+    cy.get(".MuiCardHeader-content")
+    .eq(index)
+    .find("p")
+    .contains(title);
+});
+
+Cypress.Commands.add('checkMoviesLength', (index) => {
+    cy.get(".MuiCardHeader-content").should("have.length", index);
+});
+
+Cypress.Commands.add('clickReviewButton', (index) => {
+    cy.get("svg[data-testid='RateReviewIcon'").eq(index).click();
+});
+
+Cypress.Commands.add('addReview', (name, reviewContent) => {
+    cy.get("#author").clear().type(name); 
+    cy.get("#review").clear().type(reviewContent); 
+    cy.get("#select-rating").click();
+
+    cy.contains('Excellent').click()
+
+    cy.get(".MuiButtonBase-root").contains("Submit").click();
+
+    cy.get("svg[data-testid='CloseIcon'").click();
+});
+
